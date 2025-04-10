d8700df9f25e1f80e63d3187d8ae2f59f0b28886
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2025-04-10 07:02:51 -0300

    Fix building sqlite and SVT-AV1 with new zig version  - Add a clang wrapper script that make sure PATH is clean when building with the host compiler is necessary  - Update AMF, SVT-AV1 and ffmpeg to newer versions  - Update ffmpeg handbreak patches + add a patch for compatibility with new SVT-AV1 version  - Add patch for neon support on windows for SVT-AV1  - Patch SVT-AV1 avx512 flags for comptibility with zig

--- stages/50-svt-av1.sh
+++ stages/50-svt-av1.sh
@@ -4,18 +4,16 @@ echo "Download svt-av1..."
 mkdir -p svt-av1
 
 # renovate: datasource=gitlab-releases depName=AOMediaCodec/SVT-AV1
-_tag='2.3.0'
+_tag='3.0.2'
 
 curl_tar "https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v${_tag}/SVT-AV1-v${_tag}.tar.gz" svt-av1 1
 
-case "$TARGET" in
-  x86_64*)
-    ENABLE_NASM=On
-    ;;
-  aarch64*)
-    ENABLE_NASM=Off
-    ;;
-esac
+# Handbreak patches
+curl 'https://github.com/HandBrake/HandBrake/raw/f9e7678/contrib/svt-av1/A01-Enable-Neon-DotProd-and-I8MM-on-Windows.patch' \
+  | patch -F5 -lp1 -d svt-av1 -t
+
+# Add flag required by zig to compile some of the AVX512 instructions used by SVT-AV1
+sed -i '/-mavx2/a \    -mevex512' svt-av1/Source/Lib/ASM_AVX512/CMakeLists.txt
 
 # Remove some superfluous files
 rm -rf svt-av1/{Docs,Config,test,ffmpeg_plugin,gstreamer-plugin,.gitlab*}
@@ -25,6 +23,15 @@ bak_src 'svt-av1'
 
 mkdir -p svt-av1/build
 cd svt-av1/build
+
+case "$TARGET" in
+  x86_64*)
+    ENABLE_NASM=On
+    ;;
+  aarch64*)
+    ENABLE_NASM=Off
+    ;;
+esac
 
 echo "Build svt-av1..."
 cmake \
